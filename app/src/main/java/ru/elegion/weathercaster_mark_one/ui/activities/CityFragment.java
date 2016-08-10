d9bd9357c5443b0e1297d1f7203e3e3d50a98e2c
Igor Andrianov <andrianov2406@gmail.com>
2016-08-10 10:18:05 +0300

    Added random image for CityFragment.e

--- app/src/main/java/ru/elegion/weathercaster_mark_one/ui/activities/CityFragment.java
+++ app/src/main/java/ru/elegion/weathercaster_mark_one/ui/activities/CityFragment.java
@@ -1,5 +1,9 @@
 package ru.elegion.weathercaster_mark_one.ui.activities;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import ru.elegion.weathercaster_mark_one.R;
 import ru.elegion.weathercaster_mark_one.models.City;
@@ -24,6 +33,7 @@ public class CityFragment extends Fragment {
     private TextView tvHumidity;
     private TextView tvPressure;
     private TextView tvWindSpeed;
+    private ProgressDialog mProgressDialog;
 
     public City getCity() { return mCity; }
     public void setCity(City city) { mCity = city; }
@@ -61,12 +71,56 @@ public class CityFragment extends Fragment {
         tvWindSpeed = (TextView) view.findViewById(R.id.tvWindSpeed);
 
         tvCityName.setText(mCity.getName() + ", " + mCity.getCountry());
-        ivIcon.setImageBitmap(mCity.getWeatherInfo().getIconBitmap());
+        //ivIcon.setImageBitmap(mCity.getWeatherInfo().getIconBitmap());
         tvWeatherDescription.setText(mCity.getWeatherInfo().getDescription());
         tvTemperature.setText(mCity.getWeatherInfo().getTemperature() + " " + getString(R.string.temperature_units));
         tvHumidity.setText(mCity.getWeatherInfo().getHumidity() + " " + getString(R.string.humidity_units));
         tvPressure.setText(mCity.getWeatherInfo().getPressure() + " " + getString(R.string.pressure_units));
         tvWindSpeed.setText(mCity.getWeatherInfo().getWindSpeed() + " " + getString(R.string.wind_speed_units));
+
+        final DownloadImageTask downloadImage = new DownloadImageTask();
+        downloadImage.execute("http://lorempixel.com/400/200/city/");
+
         return view;
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(getActivity());
+            mProgressDialog.setMessage(getString(R.string.downloading_data));
+            mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+
+            try {
+                if (URL.length == 0){
+                    return null;
+                }
+                URL url = new URL(URL[0]);
+                HttpURLConnection connection = (HttpURLConnection) url
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(input);
+                return bitmap;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            ivIcon.setImageBitmap(result);
+            mProgressDialog.dismiss();
+        }
     }
 }
